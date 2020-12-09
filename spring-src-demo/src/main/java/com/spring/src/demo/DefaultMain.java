98f4bafd3d297f9e0d9c65d11bf729aa1fd3d253
zhangxishan <zhangxishan@hupu.com>
2020-12-09 23:49:50 +0800

    init

---
+++ spring-src-demo/src/main/java/com/spring/src/demo/DefaultMain.java
@@ -0,0 +1,18 @@
+package com.spring.src.demo;
+
+import com.spring.src.demo.bean.SpringBean;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author zxs
+ * 2020/12/9
+ */
+public class DefaultMain {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+		applicationContext.scan("com.spring");
+		SpringBean bean = applicationContext.getBean(SpringBean.class);
+		bean.sayHello();
+	}
+}
---
+++ spring-src-demo/src/main/java/com/spring/src/demo/bean/SpringBean.java
@@ -0,0 +1,16 @@
+package com.spring.src.demo.bean;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author zxs
+ * 2020/12/9
+ */
+@Component
+public class SpringBean {
+
+
+	public void sayHello(){
+		System.out.println("hello");
+	}
+}
